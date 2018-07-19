b22671fa2c30e853d65e759926c8c1cdecc3b183
manojmadhu <kammadhuranga@hotmail.com>
2018-07-19 10:21:53 +0530

    remove comment

--- rest/bcode.php
+++ rest/bcode.php
@@ -30,8 +30,7 @@
 		$stmt_validate = $barcodeManager->ValidateScanBarcode($_barcode);
 		$rCount = $stmt_validate->rowcount();//Row count
 		
-		//Check condition and insert into barcode table
-		//2018-07-18
+		//Check condition and insert into barcode table	
 		$retVal = (rCount>0) ? '0' : 
 				$setValue = $barcodeManager->InsertBarcodeData($_Iid,$_barcode,$_scantime);
 
